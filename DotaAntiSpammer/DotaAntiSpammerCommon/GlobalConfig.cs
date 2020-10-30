f4441243a57e68edbc62a1e67da0f2defd5e1e9c
e1ektr0 <e1ektr0.xyz@gmail.com>
2020-10-30 23:07:46 +0300

    Add login password to mongo db

--- DotaAntiSpammer/DotaAntiSpammerCommon/GlobalConfig.cs
+++ DotaAntiSpammer/DotaAntiSpammerCommon/GlobalConfig.cs
@@ -4,6 +4,6 @@
     {
         public static string ApiUrl { get;  } = "http://194.87.103.72:5003";
         public static string StatsUrl { get; } = "/stats";
-        public static string ConnectionString { get; } =  "mongodb://194.87.103.72:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+        public static string ConnectionString { get; } =  "mongodb://e1ekt0:secretPassword@194.87.103.72:27017/?authSource=dota2&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
     }
 }
--- dotamatchdetailsloader/db.js
+++ dotamatchdetailsloader/db.js
@@ -1,6 +1,6 @@
 module.exports  =  async function (){
     const MongoClient = require("mongodb").MongoClient;
-    const mongoClient = new MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false", { useUnifiedTopology: true });
+    const mongoClient = new MongoClient("mongodb://e1ekt0:secretPassword@194.87.103.72:27017/?authSource=dota2&readPreference=primary&appname=MongoDB%20Compass&ssl=false", { useUnifiedTopology: true });
     var connection = await mongoClient.connect();
     return connection.db("dota2");
 }
