c9c9da4b101d8bbeb85c2189c2e97b54025d8959
Maxim Feofilov <maxim.feofilov@gmail.com>
2020-11-15 21:42:35 +0300

    Remove trash

--- DotaAntiSpammer/DotaAntiSpammerUI/models/HeroConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DotaAntiSpammerNet.models
-{
-    public class HeroConfig
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string LocalName { get; set; }
-    }
-}
